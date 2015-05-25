cfb45dd8841b5226222379479df971197a836b1e
Austin Rivas <austin.rivas@dollarshaveclub.com>
2015-05-24 20:36:54 -0700

    added torch to path

---
+++ source/50_torch.sh
@@ -0,0 +1,3 @@
+export PATH=/Users/user/torch/install/bin:$PATH  # Added automatically by torch-dist
+export LD_LIBRARY_PATH=/Users/user/torch/install/lib:$LD_LIBRARY_PATH  # Added automatically by torch-dist
+export DYLD_LIBRARY_PATH=/Users/user/torch/install/lib:$DYLD_LIBRARY_PATH  # Added automatically by torch-dist
