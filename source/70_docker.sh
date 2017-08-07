3415e6e3636afd3cb5fda060eb37c8f32244673c
Austin Rivas <arivas@pushhere.com>
2017-08-07 15:36:04 -0400

    added docker machine startup functions

--- source/70_docker.sh
+++ source/70_docker.sh
@@ -2,3 +2,16 @@
 function docker-machine-create () {
 	docker-machine create --driver virtualbox default
 }
+
+function docker-machine-eval () {
+	eval "$(docker-machine env default)"
+}
+
+function docker-machine-start () {
+	docker-machine start default
+}
+
+function docker-start () {
+	docker-machine-start
+	docker-machine-eval
+}
