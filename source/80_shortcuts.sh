e4447eaa82dbf78e8587d341bdf1c624624b8576
Austin Rivas <arivas@pushhere.com>
2017-08-08 10:14:42 -0400

    added openFinderHere command

--- source/80_shortcuts.sh
+++ source/80_shortcuts.sh
@@ -7,3 +7,5 @@ alias lwrc="repos && cd lwr-commercial && ls -af"
 alias showHiddenFiles="defaults write com.apple.finder AppleShowAllFiles YES && killall Finder"
 
 alias hideHiddenFiles="defaults write com.apple.finder AppleShowAllFiles NO && killall Finder"
+
+alias openFinderHere="open -a Finder ./"
