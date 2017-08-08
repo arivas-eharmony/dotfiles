caff44eec6ae937228e6955068bdc54e54d5e625
Austin Rivas <arivas@pushhere.com>
2017-08-08 10:12:11 -0400

    added show/hide hidden files command

--- source/80_shortcuts.sh
+++ source/80_shortcuts.sh
@@ -3,3 +3,7 @@ REPO_PATH="~/Repos"
 alias repos="cd $REPO_PATH && ls -af"
 
 alias lwrc="repos && cd lwr-commercial && ls -af"
+
+alias showHiddenFiles="defaults write com.apple.finder AppleShowAllFiles YES && killall Finder"
+
+alias hideHiddenFiles="defaults write com.apple.finder AppleShowAllFiles NO && killall Finder"
