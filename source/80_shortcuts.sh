8c93e58a9a38147406d20881246be277d319b113
Austin Rivas <arivas@pushhere.com>
2017-08-09 15:15:01 -0400

    added sonnys shortcut

--- source/80_shortcuts.sh
+++ source/80_shortcuts.sh
@@ -1,11 +1,15 @@
 REPO_PATH="~/Repos"
 
-alias repos="cd $REPO_PATH && ls -af"
-
-alias lwrc="repos && cd lwr-commercial && ls -af"
-
 alias showHiddenFiles="defaults write com.apple.finder AppleShowAllFiles YES && killall Finder"
 
 alias hideHiddenFiles="defaults write com.apple.finder AppleShowAllFiles NO && killall Finder"
 
 alias openFinderHere="open -a Finder ./"
+
+# Repo Shortcuts
+
+alias repos="cd $REPO_PATH && ls -af"
+
+alias lwrc="repos && cd lwr-commercial && ls -af"
+
+alias sonnys="repos && cd sonnys-bbq && ls -af"
