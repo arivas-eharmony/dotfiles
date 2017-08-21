a677844b122b760d6a137d29bc77eb1fcc0e17ec
Austin Rivas <arivas@pushhere.com>
2017-08-21 16:18:00 -0400

    cleaned up shortcuts

--- source/80_shortcuts.sh
+++ source/80_shortcuts.sh
@@ -1,5 +1,24 @@
 export REPO_PATH=~/Repos
 
+export WP_UPLOADS_PATH=www/app/uploads/
+
+# Lakewood Ranch
+export LWR_HOST="push_dev@lwr-www1"
+export LWR_LOCAL_PATH=$REPO_PATH/lakewood-ranch
+export LWR_REMOTE_PATH="~/www.lakewoodranch.com"
+export LWR_REMOTE_PATH_PROD=$LWR_REMOTE_PATH/prod
+export LWR_REMOTE_PATH_STAGE=$LWR_REMOTE_PATH/stage
+export LWR_LOCAL_UPLOADS_PATH=$LWR_LOCAL_PATH/$WP_UPLOADS_PATH
+export LWR_REMOTE_UPLOADS_PATH_STAGE=$LWR_REMOTE_PATH_STAGE/$WP_UPLOADS_PATH
+export LWR_REMOTE_UPLOADS_PATH_PROD=$LWR_REMOTE_PATH_PROD/$WP_UPLOADS_PATH
+
+# Lakewood Ranch Commercial
+export LWRC_LOCAL_PATH=$REPO_PATH/lwr-commercial
+export LWRC_LOCAL_UPLOADS_PATH=$LWRC_LOCAL_PATH/$WP_UPLOADS_PATH
+
+# Sonny's BBQ
+export SONNYS_LOCAL_PATH=$REPO_PATH/sonnys-bbq
+
 alias showHiddenFiles="defaults write com.apple.finder AppleShowAllFiles YES && killall Finder"
 
 alias hideHiddenFiles="defaults write com.apple.finder AppleShowAllFiles NO && killall Finder"
@@ -10,11 +29,11 @@ alias openFinderHere="open -a Finder ./"
 
 alias repos="cd $REPO_PATH && ls -af"
 
-alias lwr="repos && cd lakewood-ranch && ls -af"
+alias lwr="cd $LWR_LOCAL_PATH && ls -af"
 
-alias lwrc="repos && cd lwr-commercial && ls -af"
+alias lwrc="cd $LWRC_LOCAL_PATH && ls -af"
 
-alias sonnys="repos && cd sonnys-bbq && ls -af"
+alias sonnys="cd $SONNYS_LOCAL_PATH && ls -af"
 
 # Push git deploy
 function push_git_deploy() {
@@ -23,14 +42,16 @@ function push_git_deploy() {
 	git push origin --tags
 }
 
-export LWR_HOST="push_dev@lwr-www1"
-export LWR_LOCAL_PATH=$REPO_PATH/lakewood-ranch/www/app/uploads/
-export LWR_REMOTE_PATH_PROD="$LWR_HOST:~/www.lakewoodranch.com/prod/www/app/uploads/"
-
 # Rsync commands
 function lwr_sync_up_prod() {
-	rsync -av $LWR_LOCAL_PATH $LWR_REMOTE_PATH_PROD
+	rsync -av $LWR_LOCAL_UPLOADS_PATH $LWR_HOST:$LWR_REMOTE_PATH_PROD
 }
 function lwr_sync_down_prod() {
-	rsync -av $LWR_REMOTE_PATH_PROD $LWR_LOCAL_PATH
+	rsync -av $LWR_HOST:$LWR_REMOTE_PATH_PROD $LWR_LOCAL_UPLOADS_PATH
+}
+function lwr_sync_up_stage() {
+	rsync -av $LWR_LOCAL_UPLOADS_PATH $LWR_HOST:$LWR_REMOTE_PATH_STAGE
+}
+function lwr_sync_down_stage() {
+	rsync -av $LWR_HOST:$LWR_REMOTE_PATH_STAGE $LWR_LOCAL_UPLOADS_PATH
 }
